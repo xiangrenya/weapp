a789da04b35c4e61c27b768f7327e8d01dd4ec4f
allen.xiang <allen.xiang@gaiaworks.cn>
2019-02-01 17:03:38 +0800

    千分位

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 exports.toThousands = function(val) {
-  const str = parseFloat(val).toFixed(2);
+  const str = parseFloat(val);
   const reg = /(\d{1,3})(?=(\d{3})+(?:\.))/g;
   return str.replace(reg, '$1,');
 };
